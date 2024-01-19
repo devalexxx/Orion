2214d74b1798e5d41fae85aaa6ad8fd8738d08bf
alspin8 <46360108+alspin8@users.noreply.github.com>
2024-01-19 15:04:10 +0100

    Window creation unittest (for other os test) arm.

--- test/graphics/test_window.cpp
+++ test/graphics/test_window.cpp
@@ -2,15 +2,18 @@
 // Created by Alex on 12/11/2023.
 //
 
-//#include "doctest/doctest.h"
+#include "doctest/doctest.h"
 
-//#include "core.h"
-//
-//TEST_SUITE("Window") {
-//
-//    TEST_CASE("Creation") {
-//        auto w = orion::Window::create("test");
-//        w->run();
-//    }
-//
-//}
+#include "graphics.h"
+
+TEST_SUITE("graphics") {
+
+    TEST_CASE("window") {
+
+        SUBCASE("creation") {
+            auto w = orion::Window::create("");
+            w->close();
+        }
+    }
+
+}
