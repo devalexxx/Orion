1ad962e6a285951e9f37340a50e1c278f28d9a36
alspin8 <46360108+alspin8@users.noreply.github.com>
2024-02-12 17:10:04 +0100

    Windows fix (__PRETTY_FUNCTION__ -> __FUNCSIG__ for MSVC).

--- include/orion/core/Assert.h
+++ include/orion/core/Assert.h
@@ -8,12 +8,18 @@
 #include "fmt/core.h"
 #include "fmt/color.h"
 
-#define ORION_ASSERT_IMPL(expr, format, ...)                                  \
-    (!(expr)) ?                                                               \
-        (fmt::print(stderr, fg(fmt::color::red), "Assertion failed at {}:{} : {}\n", \
-        __FILE__, __LINE__, __PRETTY_FUNCTION__),                             \
-        fmt::print(stderr, fg(fmt::color::orange), "\tassert  : \"{}\"\n\tmessage : ", #expr),                                  \
-        fmt::print(stderr, fg(fmt::color::orange), format, ##__VA_ARGS__),                              \
+#ifdef _MSC_VER
+#   define PRETTY_FUNCTION __FUNCSIG__
+#else
+#   define PRETTY_FUNCTION __PRETTY_FUNCTION__
+#endif
+
+#define ORION_ASSERT_IMPL(expr, format, ...)                                                    \
+    (!(expr)) ?                                                                                 \
+        (fmt::print(stderr, fg(fmt::color::red), "Assertion failed at {}:{} : {}\n",            \
+        __FILE__, __LINE__, PRETTY_FUNCTION),                                                   \
+        fmt::print(stderr, fg(fmt::color::orange), "\tassert  : \"{}\"\n\tmessage : ", #expr),  \
+        fmt::print(stderr, fg(fmt::color::orange), format, ##__VA_ARGS__),                      \
         fmt::print(stderr, "\n"), abort(), 0) : 1
 
 #ifdef ORION_DEBUG
